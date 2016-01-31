19714e854133540fa0cd78e52b3a6a847a906cf3
Alejandro Rojas <ale@trixandtrax.com>
2016-01-31 19:50:41 -0300

    second commit

--- application_lulu.py
+++ application_lulu.py
@@ -68,4 +68,4 @@ def next_lulu2():  #can't have two functions with the same name
     return redirect('/main_lulu')
 
 if __name__ == "__main__":
-    app_lulu.run(debug=True)
+    app_lulu.run(port=33507)
